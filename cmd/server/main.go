15be72ac3da607189b84a25c19a651e0d6eb5d83
Stefan Antoni <stefan.antoni@deutschebahn.com>
2021-09-21 17:36:10 +0200

    Main loop

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,18 +14,20 @@ import (
 func main() {
 	term := make(chan os.Signal, 1)
 	signal.Notify(term, os.Interrupt, syscall.SIGTERM)
-	select {
-	case <-term:
-		log.Println("Received SIGTERM, exiting gracefully...")
-		os.Exit(0)
-	default:
-		t := timekeeper.New("work", time.Duration(time.Minute*25))
-		t.Start()
-		fmt.Printf("Begin: %v\n", t.Begin)
-		fmt.Printf("End: %v\n", t.End)
-		fmt.Printf("Elapsed: %v\n", t.TimeElapsed())
-		fmt.Printf("Left: %v\n", t.TimeLeft())
-		fmt.Printf("Timer done: %v\n", t.Expired())
-	}
 
+	for {
+		select {
+		case <-term:
+			log.Println("Received SIGTERM, exiting gracefully...")
+			os.Exit(0)
+		default:
+			t := timekeeper.New("work", time.Duration(time.Minute*25))
+			t.Start()
+			fmt.Printf("Begin: %v\n", t.Begin)
+			fmt.Printf("End: %v\n", t.End)
+			fmt.Printf("Elapsed: %v\n", t.TimeElapsed())
+			fmt.Printf("Left: %v\n", t.TimeLeft())
+			fmt.Printf("Timer done: %v\n", t.Expired())
+		}
+	}
 }
